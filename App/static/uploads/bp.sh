a3dd337872578931ac7625a3ffbd2ea4855b3a6b
ArthurXu <qingyu.xu@gmail.com>
2018-06-09 22:10:03 +0800

    script updated

---
+++ App/static/uploads/bp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PARAM      "host"              "127.0.0.1"         1           "Host Name"
+#PARAM      "port"              "22"                0           "Port Number"
+
+
+echo "IMAGE ClientName Data1 Data2 Version BackupID"
+echo "IMAGE rhel-guest.nbappliance.lab 0 1 12 rhel-guest.nbappliance.lab_1464611509"
+
+
+# return code
+echo 111
+# return message
+echo "this is a test"
