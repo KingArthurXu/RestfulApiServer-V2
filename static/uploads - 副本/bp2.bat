f0ed2188f8e491609dc54ba23f6470cb79706ba3
ArthurXu <3540710@qq.com>
2018-09-29 17:12:48 +0800

    20180929

--- static/uploads - 副本/bp2.bat
+++
@@ -1,9 +0,0 @@
-::# anything
-::###PARAM     Name               Default             Required    Help
-::#PARAM      "host"          "127.0.0.1"         1           "Host Name"
-::#PARAM      "port"              "22"                0           "Port Number"
-
-@echo off
-echo HELLO WORLD!
-
-
