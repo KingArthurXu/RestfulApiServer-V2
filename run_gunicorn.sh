7860acdb21027a589ec27dfe1f70e1e49073df0b
ArthurXu <3540710@qq.com>
2018-09-29 17:19:34 +0800

    Gunicorn docker file

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,14 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-gunicorn -c gunicorn_frun.py frun:app
+/usr/bin/gunicorn -c gunicorn_frun.py frun:app
+/usr/bin/gunicorn -c gunicorn_brun.py brun:app
 
-gunicorn -w 4 -b 0.0.0.0:5000 --log-level debug --daemon frun:app --access-logfile '-'
-
-gunicorn -w 4 -b 0.0.0.0:5000 frun:app
-
-gunicorn -w 1 -b 0.0.0.0:5001 --log-level debug brun:app --access-logfile -
-
-pip freeze > requirements.txt
-
-uwsgi --socket 0.0.0.0:5000 --wsgi-file frun.py --callable app --processes 4 --threads 2 --stats 0.0.0.0:9191
+#gunicorn -w 4 -b 0.0.0.0:5000 --log-level debug --daemon frun:app --access-logfile '-'
+#gunicorn -w 4 -b 0.0.0.0:5000 frun:app
+#gunicorn -w 1 -b 0.0.0.0:5001 --log-level debug brun:app --access-logfile -
+#pip freeze > requirements.txt
+#uwsgi --socket 0.0.0.0:5000 --wsgi-file frun.py --callable app --processes 4 --threads 2 --stats 0.0.0.0:9191
 
