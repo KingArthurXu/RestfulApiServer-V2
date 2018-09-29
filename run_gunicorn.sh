94c1a373a5ce9f96926faf78f93f8855054f807e
ArthurXu <3540710@qq.com>
2018-09-29 17:21:38 +0800

    Gunicorn docker file

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/gunicorn -c gunicorn_frun.py frun:app
 /usr/bin/gunicorn -c gunicorn_brun.py brun:app
-
+/usr/bin/tail -f ./log/baas.log
 #gunicorn -w 4 -b 0.0.0.0:5000 --log-level debug --daemon frun:app --access-logfile '-'
 #gunicorn -w 4 -b 0.0.0.0:5000 frun:app
 #gunicorn -w 1 -b 0.0.0.0:5001 --log-level debug brun:app --access-logfile -
